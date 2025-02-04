8b54fa58089ad2e4a5a33ba6832d192ed0b8e3cb
DShaulov <shaulovl55@gmail.com>
2025-02-04 21:23:51 +0200

    Working on search

---
+++ PeleGuide/SearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeleGuide
+{
+    public class PdfSearchResult
+    {
+        public string FilePath { get; set; }
+        public int PageNumber { get; set; }
+        public string MatchedText { get; set; }
+        public string Context { get; set; }
+    }
+}
